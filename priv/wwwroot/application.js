0d83b0386426d2dcd791bd7385d2c871a6243408
Max Lapshin <max@maxidoors.ru>
2011-10-29 01:41:22 +0400

    wwwroot

---
+++ priv/wwwroot/application.js
@@ -0,0 +1,20 @@
+function runComet() {
+  $.ajax({
+    url: "/comet",
+    type: 'post',
+    data: {},
+    cache: false,
+    success: function(data, textStatus) {
+      eval(data);
+      if(console) console.log("Restarting comet after success");
+      setTimeout(runComet, 50);
+    },
+    
+    error: function(xmlHttpRequest, textStatus, errorThrown) {
+      if(console) console.log("Restarting comet after error");
+      setTimeout(runComet, 50);
+    }
+  });
+}
+
+$(runComet);
